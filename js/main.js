dc7ce4fc3d5fce0f12dbd106adf0c7ea8970a841
nishapathak13 <nish.pathak12@gmail.com>
2021-06-06 23:31:10 +0545

    html n css changes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,35 @@ $('.logo-slider').slick({
     arrow:true,
     autoplay:true,
     autoplaSpeed:2000,
-    infinite:true
+    infinite:true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 $('.testimonial-slider').slick({
     slidesToShow: 1,
