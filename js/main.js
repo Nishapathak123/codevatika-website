56ffa1058e3b9f899ddd3f18b09e21f2ef0ae289
nishapathak13 <nish.pathak12@gmail.com>
2021-06-06 21:57:55 +0545

    desktop completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// 
+$('.logo-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots:true,
+    arrow:true,
+    autoplay:true,
+    autoplaSpeed:2000,
+    infinite:true
+});
+$('.testimonial-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:true,
+    arrow:true,
+    autoplay:true,
+    autoplaSpeed:2000,
+    infinite:true
+});
+
+
